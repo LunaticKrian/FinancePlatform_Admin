24dc2c032dd79053d5ef6d31e3145f58a86eabdb
Krian <2793260947@qq.com>
2022-09-28 22:06:01 +0800

    项目基本完成开发，进入代码评审阶段 —— 2022/09/28

--- src/api/core/integral-grade.js
+++ src/api/core/integral-grade.js
@@ -1,24 +1,25 @@
-// 在utils包下的request.js模块封装了axios功能，引入模块：
+//引入axios的初始化模块
 import request from '@/utils/request'
 
-// 导出默认模块：
+//导出默认模块
 export default {
-  // 获取积分等级列表：
+  //定义模块成员
+  //成员方法：获取积分等级列表
   list() {
+    //调用axios的初始化模块，发送远程ajax请求
     return request({
-      // 配置了统一的baseURL：http://localhost
       url: '/admin/core/integralGrade/list',
       method: 'get'
     })
   },
-  // 根据Id删除积分等级信息：
+
   removeById(id) {
     return request({
       url: '/admin/core/integralGrade/remove/' + id,
       method: 'delete'
     })
   },
-  // 新增积分等级数据：
+
   save(integralGrade) {
     return request({
       url: '/admin/core/integralGrade/save',
@@ -26,14 +27,14 @@ export default {
       data: integralGrade
     })
   },
-  // 根据ID获取积分等级信息：
+
   getById(id) {
     return request({
       url: '/admin/core/integralGrade/get/' + id,
       method: 'get'
     })
   },
-  // 根据ID更新积分等级信息：
+
   updateById(integralGrade) {
     return request({
       url: '/admin/core/integralGrade/update',
--- src/router/index.js
+++ src/router/index.js
@@ -180,43 +180,6 @@ export const constantRoutes = [
         meta: { title: '数据字典' }
       }
     ]
-  },
-
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: { title: '例子', icon: 'el-icon-s-help' },
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: '表格', icon: 'table' }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: { title: '树', icon: 'tree' }
-      }
-    ]
-  },
-
-  {
-    path: '/form',
-    component: Layout,
-    meta: { title: '一级标题', icon: 'el-icon-goods' },
-    alwaysShow: true,
-    children: [
-      {
-        path: 'index',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: '表单', icon: 'form' }
-      }
-    ]
   }
 ]
 
@@ -226,72 +189,12 @@ export const constantRoutes = [
  */
 export const asyncRoutes = [
   {
-    path: '/nested',
-    component: Layout,
-    redirect: '/nested/menu1',
-    name: 'Nested',
-    meta: {
-      title: 'Nested',
-      icon: 'nested'
-    },
-    children: [
-      {
-        path: 'menu1',
-        component: () => import('@/views/nested/menu1/index'), // Parent router-view
-        name: 'Menu1',
-        meta: { title: 'Menu1' },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/nested/menu1/menu1-1'),
-            name: 'Menu1-1',
-            meta: { title: 'Menu1-1' }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/nested/menu1/menu1-2'),
-            name: 'Menu1-2',
-            meta: { title: 'Menu1-2' },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () =>
-                  import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-                name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () =>
-                  import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-                name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/nested/menu1/menu1-3'),
-            name: 'Menu1-3',
-            meta: { title: 'Menu1-3' }
-          }
-        ]
-      },
-      {
-        path: 'menu2',
-        component: () => import('@/views/nested/menu2/index'),
-        meta: { title: 'menu2' }
-      }
-    ]
-  },
-
-  {
     path: 'external-link',
     component: Layout,
     children: [
       {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' }
+        path: 'http://localhost:3000/',
+        meta: { title: '主站点', icon: 'link' }
       }
     ]
   },
